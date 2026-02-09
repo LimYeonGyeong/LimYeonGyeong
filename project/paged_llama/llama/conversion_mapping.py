df7cf4e260a82b091c3162d0237b3e251636dfcc
LimYeonGyeong <87985547+LimYeonGyeong@users.noreply.github.com>
2026-02-10 04:33:40 +0900

    .

---
+++ project/paged_llama/llama/conversion_mapping.py
@@ -0,0 +1,8 @@
+# paged_llama/llama/conversion_mapping.py
+
+def get_model_conversion_mapping(config):
+    """
+    모델 가중치 변환 맵핑을 반환하는 더미 함수입니다.
+    현재는 에러 방지를 위해 빈 딕셔너리를 반환합니다.
+    """
+    return {}
